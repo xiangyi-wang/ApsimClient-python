5272670512c7156bad5f56a4774660945c90353b
xiangyi wang <2206037943@qq.com>
2021-10-21 16:15:04 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ApsimCli',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='xiangyiwang',
+    author_email='',
+    description=''
+)
