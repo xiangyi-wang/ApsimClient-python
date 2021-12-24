819c38aa743ea2c6bde6d995c55b54157e28a5ec
xiangyi wang <2206037943@qq.com>
2021-12-24 16:28:04 +0800

    1.fixes a bug when reading characters from server 2.add script to create.apsimx file 3.Provides an example of change cultivar parameters

--- ApsimClient.py
+++ ApsimClient.py
@@ -5,6 +5,7 @@
 # @Software: PyCharm
 import os.path
 import sys
+import random
 from ctypes import c_int32,sizeof,c_double,c_char,c_int64
 import struct
 import socket
@@ -22,11 +23,11 @@ PROPERTY_TYPE_DOUBLE = 1
 PROPERTY_TYPE_BOOL = 2
 PROPERTY_TYPE_DATE = 3
 PROPERTY_TYPE_STRING = 4
-PROPERTY_TYPE_INT_ARRAY = 5
-PROPERTY_TYPE_DOUBLE_ARRAY = 6
-PROPERTY_TYPE_BOOL_ARRAY = 7
-PROPERTY_TYPE_DATE_ARRAY = 8
-PROPERTY_TYPE_STRING_ARRAY = 9
+# PROPERTY_TYPE_INT_ARRAY = 5
+# PROPERTY_TYPE_DOUBLE_ARRAY = 6
+# PROPERTY_TYPE_BOOL_ARRAY = 7
+# PROPERTY_TYPE_DATE_ARRAY = 8
+# PROPERTY_TYPE_STRING_ARRAY = 9
 def connect_to_remote_server(ip_address, port):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
@@ -93,7 +94,15 @@ def send_replacement_to_socket(socket,change):
     send_to_socket(socket, change["paramtype"], sizeof(c_int32))
     validate_response(socket, ACK)
     # 3. Send the parameter itself.
-    send_to_socket(socket, change["value"], sizeof(c_double))
+    if isinstance(change["value"],float):
+        typeofvalue = c_double
+        send_to_socket(socket, change["value"], sizeof(typeofvalue))
+    elif isinstance(change["value"],int):
+        typeofvalue = c_int32
+        send_to_socket(socket, change["value"], sizeof(typeofvalue))
+    elif isinstance(change["value"],str):
+        send_string_to_socket(socket, change["value"])
+
     validate_response(socket, ACK)
 def run_with_changes(socket,changes):
     send_string_to_socket(socket, COMMAND_RUN)
@@ -105,7 +114,7 @@ def run_with_changes(socket,changes):
     send_string_to_socket(socket, FIN)
     validate_response(socket, ACK)
     validate_response(socket, FIN)
-    print('run finished')
+    print('Run Finished')
 
 def read_output(socket,tablename,param_list):
     # 1. Send READ command.
@@ -124,33 +133,46 @@ def read_output(socket,tablename,param_list):
     # Send FIN to indicate end of parameter names.
     send_string_to_socket(socket, FIN)
     send_string_to_socket(socket, ACK)
-    validate_response(socket, FIN)
-    results={}
 
+    results={}
     for param_name,param_type in param_list.items():
         result_output_of_one = read_output_of_one(socket, param_type)
         results[param_name]=result_output_of_one
         send_string_to_socket(socket, ACK)
+
+    validate_response(socket, ACK)#这里注意
+    disconnect_from_server(socket)
     return results
 
 def read_output_of_one(socket,param_type):
     recv_size , recv_data = read_from_socket(socket)
     # print(recv_data)
     # print(recv_size)
-    result_of_all = []
-    for i in range(0, recv_size, sizeof(param_type)):
-        result_of_one = recv_data[i:i + sizeof(param_type)]
-        # print(result_of_one)
-        if param_type==c_int32:
-            # result_of_one = result_of_one.decode()
-            result_of_one = struct.unpack('@i', result_of_one)[0]
-        elif param_type==c_double:
-            result_of_one = struct.unpack('@d', result_of_one)[0]
-        elif param_type==c_char:
-            result_of_one = struct.unpack('@s', result_of_one)[0]
-        result_of_all.append(result_of_one)
+    if param_type==c_int32:
+        # result_of_one = result_of_one.decode()
+        result_of_one = struct.unpack('@i', recv_data)[0]
+    elif param_type==c_double:
+        result_of_one = struct.unpack('@d', recv_data)[0]
+    elif param_type==c_char:
+        # result_of_one = struct.unpack('@s', recv_data)[0]
+        result_of_one = recv_data.decode("utf-8")
+    # print(result_of_one)
+    # result_of_all = []
+    # for i in range(0, recv_size, sizeof(param_type)):
+    #     result_of_one = recv_data[i:i + sizeof(param_type)]
+    #     # print(result_of_one)
+    #     if param_type==c_int32:
+    #         # result_of_one = result_of_one.decode()
+    #         result_of_one = struct.unpack('@i', result_of_one)[0]
+    #     elif param_type==c_double:
+    #         result_of_one = struct.unpack('@d', result_of_one)[0]
+    #     elif param_type==c_char:
+    #         # result_of_one = struct.unpack('@s', result_of_one)[0]
+    #         result_of_one = result_of_one.decode("utf-8")
+    #     # print(result_of_one)
+    #     result_of_all.append(result_of_one)
 
-    return result_of_all
+    return result_of_one
     # clientSoscket.close()
 def start_apsim_server(SERVER_PATH,JSON_PATH,JSON_NAME,HOST='0.0.0.0',PORT=27747,SOCKETNAME='ApsimNGServer'):
    # COMMOND_LINE = SERVER_PATH+'/apsim-server.exe'          \
@@ -179,25 +201,33 @@ def start_apsim_server(SERVER_PATH,JSON_PATH,JSON_NAME,HOST='0.0.0.0',PORT=27747
 
 
 if __name__ == '__main__':
-    SERVER_PATH=r'E:\Apsim\bin/'
-    JSON_PATH=r'E:\Apsim\Examples'
-    JSON_NAME='Wheat.apsimx'
+    SERVER_PATH=r'E:/Apsim/bin/'
+    JSON_PATH=r'E:/Apsim/Examples'
+    JSON_NAME='aaa.apsimx'
     # start_apsim_server(SERVER_PATH,JSON_PATH,JSON_NAME,PORT=27747)
 
     #C:\Users\admin>E:\APSIM\bin/apsim-server.exe --file E:\APSIM\Examples\Wheat.apsimx --verbose --native --remote --keep-alive --address 127.0.0.1 --port 27747
     # Connect to the socket.
     print("Connecting to server...")
-    sock = connect_to_remote_server("127.0.0.1", 27747)
-    print("connected\n")
+    try:
+        sock = connect_to_remote_server("127.0.0.1", 27747)
+        print("Connected\n")
+    except Exception as e:
+        print(e)
+
     changes=[
-        {"path":"[Phenology].Phyllochron.BasePhyllochron.FixedValue",
-         "value":1.5,
-         'paramtype':PROPERTY_TYPE_DOUBLE
-         }]
+
+        {"path": "[Rua].Command[3]",
+         "value": '[Structure].Phyllochron.Phyllochron.Phyllochron.XYPairs.X = 0,0.15,0.74,0.75,1',
+         'paramtype': PROPERTY_TYPE_STRING
+         },
+
+    ]
     run_with_changes(sock,changes)
     tablename='Report'
-    param_list={'Clock.Today.DayOfYear':c_int32,
+    param_list={
+                'Clock.Today.DayOfYear':c_int32,
                 'Yield':c_double}
     results=read_output(sock,tablename,param_list)
-    print(pd.DataFrame(results))
+    print(results)
 
